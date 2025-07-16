e271d41ea25c9da844b38d8d697ee75ba22cdba9
hamgeek <ham.intl.id@gmail.com>
2025-07-16 13:38:03 +0700

    update: data/project.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ export const PROJECTS = [
     title: 'TodoAI App',
     description:
       'A smart task management tool powered by AI. TodoAI helps users create, organize, and manage their to-do lists more efficiently.',
-    tags: ['Typescript', 'Next.js', 'Prisma ORM', 'Laravel'],
+    tags: ['Typescript', 'Next.js', 'Prisma ORM'],
     thumb_url: 'https://assets.ham.web.id/projects/project-todoai.png',
     preview_url: 'https://todoai-app-ham.vercel.app',
     github_url: 'https://github.com/hamgeek/todoai-app',
